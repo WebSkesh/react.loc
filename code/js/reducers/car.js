c07727a52399c170da964385564e45526b31e6ab
Peter <dev23@pruinv.local>
2019-12-30 14:29:26 +0200

    first commit

---
+++ code/js/reducers/car.js
@@ -0,0 +1,20 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            car: 'Audi',
+            speed: 234.45,
+            weight: 1.4,
+            desc: 'some desc',
+            img: 'https://audi-center.zp.ua/upload/resize_cache/iblock/b7c/291_125_2/b7cd3800f808782befee6927660beea7.png'
+        },
+        {
+            id: 2,
+            car: 'BMW',
+            speed: 334.45,
+            weight: 1.2,
+            desc: 'some desc',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHwcFcHjvpP4RQzdxIU1QVcREXbLcmfh6XuADfHryLBud6g_mf&s'
+        }
+    ];
+}
